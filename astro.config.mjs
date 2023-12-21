a9bdb2b162624ec2ab6bd617ecc681beaae7d78f
akmot9 <avicocyprien@yahoo.com>
2023-12-21 11:37:54 +0100

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	//site: 'https://example.com',
+	site: 'https://Sonar-team.github.io',
+	base: '/doc',
 	integrations: [
 		starlight({
 			title: 'My Docs',
