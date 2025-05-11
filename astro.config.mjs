e72f06ad3c59fcae0862ec3526f277ac76dfc699
cyprien <avicocyprien@yahoo.com>
2025-05-11 13:48:39 +0200

    add translataion en

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,53 +1,74 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import astroIcon from 'astro-icon';
 import rehypeMermaid from 'rehype-mermaid';
-// https://astro.build/config
+
 export default defineConfig({
-	site: 'https://Sonar-team.github.io',
+  site: 'https://Sonar-team.github.io',
+  base: '/doc',
 
-	markdown: {
-		rehypePlugins: [rehypeMermaid], 
-	  },
+  markdown: {
+    rehypePlugins: [rehypeMermaid],
+  },
 
-	base: '/doc',
-	integrations: [
-		starlight({
-			title: 'Sonar',
-			logo: {
-				src: './src/assets/logo_sonar.png',
-			},
-			social: [
-				{
-					icon: "github",
-					label: "GitHub",
-				 	href: "https://github.com/Sonar-team"
-				},
-				{
-					icon: "discord",
-					label: "Discord",
-					href: "https://discord.gg/65twx4CpDr"
-				}
-			],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Installation', link: 'guides/installation' },
-					],
-				},
-				{
-					label: 'Developpement',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Capture pcap', link: 'developpement/capture' },
-					],
-				},
-			],
-		}),
-		astroIcon(),
-	],
+  integrations: [
+    starlight({
+      // Titre multilingue
+      title: {
+        fr: 'Sonar',
+        en: 'Sonar ',
+      },
 
+      defaultLocale: 'fr',
+
+      locales: {
+        fr: { label: 'Français', lang: 'fr' },
+        en: { label: 'English', lang: 'en' },
+      },
+
+      logo: {
+        src: './src/assets/logo_sonar.png',
+      },
+
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/Sonar-team',
+        },
+        {
+          icon: 'discord',
+          label: 'Discord',
+          href: 'https://discord.gg/65twx4CpDr',
+        },
+      ],
+
+      sidebar: [
+        {
+          label: 'Guides',
+          translations: { en: 'Guides' },
+          items: [
+            {
+              label: 'Installation',
+              translations: { en: 'Installation' },
+              link: 'guides/installation',
+            },
+          ],
+        },
+        {
+          label: 'Développement',
+          translations: { en: 'Development' },
+          items: [
+            {
+              label: 'Capture pcap',
+              translations: { en: 'Pcap Capture' },
+              link: 'developpement/capture',
+            },
+          ],
+        },
+      ],
+    }),
+
+    astroIcon(),
+  ],
 });
